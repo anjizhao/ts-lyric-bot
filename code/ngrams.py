c931d4f20521e662da728369c2f7abfc59cdf9e9
anji <anji6789@gmail.com>
2021-03-12 15:47:08 -0500

    clean up and fixes.

--- code/ngrams.py
+++ code/ngrams.py
@@ -6,12 +6,9 @@ from typing import List, Optional
 
 
 from nltk.lm.models import Laplace
-from nltk.lm.preprocessing import (
-    flatten, pad_both_ends,  # padded_everygrams, padded_everygram_pipeline,
-)
+from nltk.lm.preprocessing import flatten, pad_both_ends
 from nltk.tokenize import word_tokenize
-# from nltk.tokenize.treebank import TreebankWordDetokenizer
-from nltk.util import everygrams  # , ngrams
+from nltk.util import everygrams
 
 
 text = '''There's something 'bout the way
@@ -27,30 +24,44 @@ Absent-mindedly making me want you'''
 
 
 class MyNGram:
-    def __init__(self, n: int, interpolate: bool = True) -> None:
+    def __init__(
+        self,
+        n: int,
+        include_lower_order_grams: bool = True,
+        start_token: str = '<s>',
+        end_token: str = '</s>',
+    ) -> None:
 
         # params
         self.n = n
-        self.interpolate = interpolate
+        self.include_lower_order_grams = include_lower_order_grams
 
-        # the actual model
+        self.start_token = start_token
+        self.end_token = end_token
+
+        # the actual nltk model
         self.model = Laplace(n)
 
+        # function to be used to break a list of strs into "grams"
         self.make_grams = partial(
-            # function to be used to break a list of strs into "grams"
             everygrams,
-            min_len=1 if interpolate else n,  # i think??
+            min_len=1 if include_lower_order_grams else n,
             max_len=n,
         )
+
 
     def tokenize(self, text: str) -> List[str]:
         return list(word_tokenize(text.lower()))
 
-    def _pad(self, tokens: List[str]) -> List[str]:
-        return list(pad_both_ends(tokens, self.n))
 
     def padded_tokenize(self, text: str) -> List[str]:
-        return self._pad(self.tokenize(text))
+        return list(pad_both_ends(
+            self.tokenize(text),
+            self.n,
+            left_pad_symbol=self.start_token,
+            right_pad_symbol=self.end_token,
+        ))
+
 
     def train(self, sentences: List[List[str]]) -> None:
         # input sentences should be tokenized and padded already
@@ -58,39 +69,35 @@ class MyNGram:
         vocab = flatten(sentences)
         self.model.fit(training_data, vocab)
 
+
     def prep_test_text(self, text: str) -> List[str]:
         ''' convert test string into list of grams '''
         return list(self.make_grams(self.padded_tokenize(text)))
+
 
     def test_text_entropy(self, text: str) -> float:
         test_grams = self.prep_test_text(text)
         return self.model.entropy(test_grams)
 
+
     def test_text_perplexity(self, text: str) -> float:
         test_grams = self.prep_test_text(text)
         return self.model.perplexity(test_grams)
 
-    def test_text_log_score(self, text: str) -> float:
-        test_grams = self.prep_test_text(text)
-        entropy = self.model.entropy(test_grams)
-        # entropy is negative of the average log likelihood
-        return (-1) * entropy * len(test_grams)
-
-    def test_text_likelihood(self, text: str) -> float:
-        return 2 ** self.test_text_log_score(text)
 
     def generate(
         self, num_words: int, context: Optional[List[str]] = None,
     ) -> List[str]:
         if context is None:
+            # start with "sentence start"
             context = self.padded_tokenize('')[:self.n - 1]
         return self.model.generate(num_words, context)
 
 
 if __name__ == '__main__':
-    ngram_a = MyNGram(1, interpolate=False)
-    ngram_b = MyNGram(2, interpolate=False)
-    ngram_c = MyNGram(2, interpolate=True)
+    ngram_a = MyNGram(1, include_lower_order_grams=False)
+    ngram_b = MyNGram(2, include_lower_order_grams=False)
+    ngram_c = MyNGram(2, include_lower_order_grams=True)
     print('ngram_a (1-grams)')
     print('ngram_b (2-grams)')
     print('ngram_c (1-grams & 2-grams)')
@@ -109,12 +116,12 @@ if __name__ == '__main__':
     ]
     for test_str in test_strings:
         print('test str:', test_str)
-        print('ngram_a log_score: {:.4f}'.format(ngram_a.test_text_log_score(test_str)))  # noqa
-        print('ngram_b log_score: {:.4f}'.format(ngram_b.test_text_log_score(test_str)))  # noqa
-        print('ngram_c log_score: {:.4f}'.format(ngram_c.test_text_log_score(test_str)))  # noqa
+        print('ngram_a entropy: {:.4f}'.format(ngram_a.test_text_entropy(test_str)))  # noqa
+        print('ngram_b entropy: {:.4f}'.format(ngram_b.test_text_entropy(test_str)))  # noqa
+        print('ngram_c entropy: {:.4f}'.format(ngram_c.test_text_entropy(test_str)))  # noqa
         print('')
 
-    print(ngram_a.generate(5))
-    print(ngram_b.generate(5))
-    print(ngram_c.generate(5))
+    print(ngram_a.generate(3))
+    print(ngram_b.generate(3))
+    print(ngram_c.generate(3))
 
