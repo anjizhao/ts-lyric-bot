003e41298894bd9e7f431462cf766738f5c4f2ca
anji <anji6789@gmail.com>
2021-03-17 19:46:03 -0400

    find ideal hYpErpArAmeTeRs. more figs 🍇.

--- code/model_selection.py
+++ code/model_selection.py
@@ -1,5 +1,6 @@
 
 import csv
+import glob
 from statistics import mean, stdev
 import time
 from typing import Any, Dict, List, NamedTuple
@@ -11,7 +12,6 @@ from nltk.lm.models import (
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
-import tqdm
 
 from code.ngrams import MyNGram
 
@@ -25,12 +25,13 @@ class LMDef(NamedTuple):
 
 test_orders = [1, 2, 3, 4]
 # lidstone_test_alphas = [0.001, 0.01, 0.1, 1, 5]
-# lidstone_test_alphas = np.linspace(0, 1, 11)[1:]  # 0-1 excluding 0
-lidstone_test_alphas = np.linspace(0, 0.005, 11)[1:]  # 0-0.005 excluding 0
+lidstone_test_alphas = np.linspace(0, 1, 11)[1:]  # 0-1 excluding 0
+# lidstone_test_alphas = np.linspace(0, 0.001, 11)[1:]  # 0-0.005 excluding 0
 
 # kn_test_discounts = [0.001, 0.01, 0.1, 1]
 # kn_test_discounts = np.linspace(0, 1, 11)[1:]  # 0-1 excluding 0
 kn_test_discounts = np.linspace(0.1, 0.6, 11)  # 0.1-0.6 inclusive
+# kn_test_discounts = np.linspace(0.2, 0.6, 21)  # 0.2-0.6 excl. 0
 
 
 lidstone_models = [
@@ -150,7 +151,7 @@ def read_csv_to_df(filename: str) -> pd.DataFrame:
 
 def plot_lidstone(df):
     cmap = plt.get_cmap('viridis')
-    fig, axes = plt.subplots(1, 2, sharey=True, figsize=(10, 5))
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
     orders = sorted(set(df.order))
     alphas = sorted(set(df.alpha))
     for j, metric in enumerate(['train_entropy_mean', 'test_entropy_mean']):
@@ -165,9 +166,9 @@ def plot_lidstone(df):
                 color=cmap(order / 4),
             )
         axes[j].set_title(metric)
-        axes[j].set_xticks(alphas)
+        # axes[j].set_xticks(alphas)
         axes[j].set_ylabel('entropy')
-        axes[j].legend(loc='lower right')
+        # axes[j].legend(loc='lower right')
     plt.suptitle('lidstone hyperparameters')
     plt.show()
 
@@ -189,30 +190,92 @@ def plot_kn(df):
                 color=cmap(order / 4),
             )
         axes[j].set_title(metric)
-        axes[j].set_xticks(discounts)
+        # axes[j].set_xticks(discounts)
         axes[j].set_ylabel('entropy')
         # axes[j].legend(loc='upper left')
     plt.suptitle('kneser-ney hyperparameters')
     plt.show()
 
 
+def compile_all_lidstone() -> pd.DataFrame:
+    dfs = []
+    for lidstone_filename in glob.glob('data/model_selection_lidstone_*.csv'):
+        df = read_csv_to_df(lidstone_filename)
+        dfs.append(df)
+    return pd.concat(
+        dfs,
+        ignore_index=True,
+    ).drop_duplicates(
+        subset=['order', 'alpha'], keep='last',
+    ).reset_index(drop=True)
+
+
+def compile_all_kn() -> pd.DataFrame:
+    dfs = []
+    for kn_filename in glob.glob('data/model_selection_kn_*.csv'):
+        df = read_csv_to_df(kn_filename)
+        dfs.append(df)
+    return pd.concat(
+        dfs,
+        ignore_index=True,
+    ).drop_duplicates(
+        subset=['order', 'discount'], keep='last',
+    ).reset_index(drop=True)
+
+
+def get_min_per_order(
+    input_df: pd.DataFrame,
+    column: str = 'test_entropy_mean',
+) -> pd.DataFrame:
+    min_indices = []
+    for group, df in input_df.groupby('order'):
+        min_index = df[column].idxmin()
+        min_indices.append(min_index)
+    return input_df.iloc[min_indices]
+
+
 
 if __name__ == '__main__':
     training_corpus = get_train_set()
+
     # lidstone_results = kfold_validation(
     #     lidstone_models, training_corpus, n_splits=4,
     # )
     # lidstone_filename = write_lidstone_results_to_file(lidstone_results)
 
-    # lidstone_filename = 'data/model_selection_lidstone_1616008852.csv'
-    # lidstone_df = read_csv_to_df(lidstone_filename)
-    # plot_lidstone(lidstone_df)
+    # # lidstone_filename = 'data/model_selection_lidstone_1616008852.csv'
+    # # lidstone_df = read_csv_to_df(lidstone_filename)
+    lidstone_df = compile_all_lidstone()
+    plot_lidstone(lidstone_df)
+    print(get_min_per_order(lidstone_df))
 
     # kn_results = kfold_validation(
     #     kn_models, training_corpus, n_splits=4,
     # )
     # kn_filename = write_kn_results_to_file(kn_results)
 
-    kn_filename = 'data/model_selection_kn_1616015340.csv'
-    kn_df = read_csv_to_df(kn_filename)
+    # kn_filename = 'data/model_selection_kn_1616024156.csv'
+    # kn_df = read_csv_to_df(kn_filename)
+    kn_df = compile_all_kn()
     plot_kn(kn_df)
+    print(get_min_per_order(kn_df))
+
+
+'''
+
+$ python code/model_selection.py
+(params minimizing test_entropy_mean for each order)
+
+        model      order     alpha  train_entropy_mean  test_entropy_mean   ...
+78   Lidstone          1    1.0000            8.423954           8.511394   ...
+112  Lidstone          2    0.0040            4.805368           6.489021   ...
+34   Lidstone          3    0.0006            2.480749           5.965648   ...
+23   Lidstone          4    0.0002            1.557747           5.899271   ...
+
+        model      order  discount  train_entropy_mean  test_entropy_mean   ...
+41  KneserNeyInt.      1      0.20           11.402517          11.402517   ...
+48  KneserNeyInt.      2      0.50            4.659039           6.263645   ...
+99  KneserNeyInt.      3      0.42            2.383805           5.761089   ...
+79  KneserNeyInt.      4      0.28            1.523350           5.751098   ...
+
+'''
