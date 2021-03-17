b169a3e1fb42e77fdec545b939747e884846abfc
anji <anji6789@gmail.com>
2021-03-17 17:22:53 -0400

    mOre model selection stuff. add DIAGRAMs!

--- code/model_selection.py
+++ code/model_selection.py
@@ -4,10 +4,12 @@ from statistics import mean, stdev
 import time
 from typing import Any, Dict, List, NamedTuple
 
+import matplotlib.pyplot as plt
 from nltk.lm.models import (
     LanguageModel, Lidstone, KneserNeyInterpolated,
 )
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import KFold
 import tqdm
 
@@ -21,9 +23,14 @@ class LMDef(NamedTuple):
     kwargs: Dict[str, Any] = dict()
 
 
-test_orders = [1, 2, 3]
-lidstone_test_alphas = [0.001, 0.01, 0.1, 1, 5]
-kn_test_discounts = [0.001, 0.01, 0.1, 1]
+test_orders = [1, 2, 3, 4]
+# lidstone_test_alphas = [0.001, 0.01, 0.1, 1, 5]
+# lidstone_test_alphas = np.linspace(0, 1, 11)[1:]  # 0-1 excluding 0
+lidstone_test_alphas = np.linspace(0, 0.005, 11)[1:]  # 0-0.005 excluding 0
+
+# kn_test_discounts = [0.001, 0.01, 0.1, 1]
+# kn_test_discounts = np.linspace(0, 1, 11)[1:]  # 0-1 excluding 0
+kn_test_discounts = np.linspace(0.1, 0.6, 11)  # 0.1-0.6 inclusive
 
 
 lidstone_models = [
@@ -96,8 +103,8 @@ def kfold_validation(
     return results
 
 
-def write_lidstone_results_to_file(results: Dict[str, Any]) -> None:
-    filename = 'data/model_selection_lidstone_{}.txt'.format(int(time.time()))
+def write_lidstone_results_to_file(results: List[Dict[str, Any]]) -> str:
+    filename = 'data/model_selection_lidstone_{}.csv'.format(int(time.time()))
     with open(filename, 'w') as csvfile:
         writer = csv.DictWriter(
             csvfile,
@@ -112,10 +119,11 @@ def write_lidstone_results_to_file(results: Dict[str, Any]) -> None:
         for r in results:
             r['alpha'], r['order'] = r.pop('args')
             writer.writerow(r)
+    return filename
 
 
-def write_kn_results_to_file(results: Dict[str, Any]) -> None:
-    filename = 'data/model_selection_kn_{}.txt'.format(int(time.time()))
+def write_kn_results_to_file(results: List[Dict[str, Any]]) -> str:
+    filename = 'data/model_selection_kn_{}.csv'.format(int(time.time()))
     with open(filename, 'w') as csvfile:
         writer = csv.DictWriter(
             csvfile,
@@ -131,17 +139,80 @@ def write_kn_results_to_file(results: Dict[str, Any]) -> None:
             r['order'] = r['args'][0]
             r['discount'] = r['kwargs'].get('discount')
             writer.writerow(r)
+    return filename
+
+
+def read_csv_to_df(filename: str) -> pd.DataFrame:
+    with open(filename, 'r') as csvfile:
+        df = pd.read_csv(csvfile)
+    return df
+
+
+def plot_lidstone(df):
+    cmap = plt.get_cmap('viridis')
+    fig, axes = plt.subplots(1, 2, sharey=True, figsize=(10, 5))
+    orders = sorted(set(df.order))
+    alphas = sorted(set(df.alpha))
+    for j, metric in enumerate(['train_entropy_mean', 'test_entropy_mean']):
+        for i, order in enumerate(orders):
+            df[df.order == order].plot(
+                'alpha',
+                metric,
+                kind='scatter',
+                label='order={}'.format(order),
+                legend=True,
+                ax=axes[j],
+                color=cmap(order / 4),
+            )
+        axes[j].set_title(metric)
+        axes[j].set_xticks(alphas)
+        axes[j].set_ylabel('entropy')
+        axes[j].legend(loc='lower right')
+    plt.suptitle('lidstone hyperparameters')
+    plt.show()
+
+
+def plot_kn(df):
+    cmap = plt.get_cmap('viridis')
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    orders = sorted(set(df.order))
+    discounts = sorted(set(df.discount))
+    for j, metric in enumerate(['train_entropy_mean', 'test_entropy_mean']):
+        for i, order in enumerate(orders):
+            df[df.order == order].plot(
+                'discount',
+                metric,
+                kind='scatter',
+                label='order={}'.format(order),
+                legend=True,
+                ax=axes[j],
+                color=cmap(order / 4),
+            )
+        axes[j].set_title(metric)
+        axes[j].set_xticks(discounts)
+        axes[j].set_ylabel('entropy')
+        # axes[j].legend(loc='upper left')
+    plt.suptitle('kneser-ney hyperparameters')
+    plt.show()
 
 
 
 if __name__ == '__main__':
     training_corpus = get_train_set()
     # lidstone_results = kfold_validation(
-    #     lidstone_models, training_corpus, n_splits=5,
+    #     lidstone_models, training_corpus, n_splits=4,
     # )
-    # write_lidstone_results_to_file(lidstone_results)
-    kn_results = kfold_validation(
-        kn_models, training_corpus, n_splits=5,
-    )
-    write_kn_results_to_file(kn_results)
+    # lidstone_filename = write_lidstone_results_to_file(lidstone_results)
 
+    # lidstone_filename = 'data/model_selection_lidstone_1616008852.csv'
+    # lidstone_df = read_csv_to_df(lidstone_filename)
+    # plot_lidstone(lidstone_df)
+
+    # kn_results = kfold_validation(
+    #     kn_models, training_corpus, n_splits=4,
+    # )
+    # kn_filename = write_kn_results_to_file(kn_results)
+
+    kn_filename = 'data/model_selection_kn_1616015340.csv'
+    kn_df = read_csv_to_df(kn_filename)
+    plot_kn(kn_df)
--- code/ngrams.py
+++ code/ngrams.py
@@ -8,6 +8,7 @@ from typing import Iterator, List, Optional
 # monkey-patched Vocabulary (needs to be imported before LanguageModel)
 from code.vocabulary_patch import Vocabulary  # noqa
 
+from nltk.lm.api import _random_generator, _weighted_choice
 from nltk.lm.models import LanguageModel, Laplace
 from nltk.lm.preprocessing import flatten, pad_both_ends
 from nltk.tokenize import word_tokenize
@@ -111,44 +112,92 @@ class MyNGram:
         for s in tqdm.tqdm(texts):
             entropies.append(self.test_text_entropy(s))
         return mean(entropies)
-        # return mean(self.test_text_entropy(s) for s in texts)
 
 
+    def generate(self, num_words=1, text_seed=None, random_seed=None):
+        '''
+        generate sentence from model. this is mostly copied from the
+        nltk.lm.api.LanguageModel.generate method, but modified to include
+        start/stop token logic.
 
-    def generate(
-        self, num_words: int, context: Optional[List[str]] = None,
-    ) -> List[str]:
-        if context is None:
-            # start with "sentence start"
-            context = [self.start_token] * (self.model.order - 1)
-        return self.model.generate(num_words, context)
+        namely, if no text_seed (context) is provided, we assume
+        this is the beginning of a sentence, so we create a list of (order - 1)
+        sentence start tokens and use that as the context.
+
+        whenever the next word generated is a stop token, we break out of the
+        loop and "stop" trying to find new words. this avoids getting the
+        `ValueError("Can't choose from empty population")` exception when a
+        sentence ends before `num_words` is reached.
+        '''
+
+        if text_seed is None:
+            text_seed = [self.start_token] * (self.model.order - 1)
+        else:
+            text_seed = list(text_seed)
+
+        random_generator = _random_generator(random_seed)
+        if num_words == 1:  # This is the base recursion case.
+            context = (
+                text_seed[-self.model.order + 1:]
+                if len(text_seed) >= self.model.order
+                else text_seed
+            )
+            samples = self.model.context_counts(
+                self.model.vocab.lookup(context)
+            )
+            while context and not samples:
+                context = context[1:] if len(context) > 1 else []
+                samples = self.model.context_counts(
+                    self.model.vocab.lookup(context)
+                )
+            # Sorting samples achieves two things:
+            # - reproducible randomness when sampling
+            # - turns Mapping into Sequence which `_weighted_choice` expects
+            return _weighted_choice(
+                sorted(samples),
+                tuple(self.model.score(w, context) for w in samples),
+                random_generator,
+            )
+
+        # We build up text one word at a time using the preceding context.
+        generated = []
+        for _ in range(num_words):
+            word = self.generate(
+                num_words=1,
+                text_seed=text_seed + generated,
+                random_seed=random_generator,
+            )
+            if word == self.end_token:
+                break  # end the sentence early if end_token is encountered
+            generated.append(word)
+
+        return generated
+
 
 
 if __name__ == '__main__':
     ngram_a = MyNGram(Laplace(1), include_lower_order_grams=False)
     ngram_b = MyNGram(Laplace(2), include_lower_order_grams=False)
-    ngram_c = MyNGram(Laplace(2), include_lower_order_grams=True)
-    print('ngram_a (1-grams)')
-    print('ngram_b (2-grams)')
-    print('ngram_c (1-grams & 2-grams)')
-    print('')
+    ngram_c = MyNGram(Laplace(3), include_lower_order_grams=False)
+
     for ngram in (ngram_a, ngram_b, ngram_c):
         ngram.train(raw_sentences=test_text.strip().split('\n'))
-    test_strings = [
-        "The street looks when it's just rained",
-        'Something about the way my street looks?',
-        'i am Taylor Alison Swift and i love CATS!',
-        'asdfasdf jlkdfjk asdf; ',
-        "I wanna stay right here, in this passenger's seat",
-    ]
-    for test_str in test_strings:
-        print('test str:', test_str)
-        print('ngram_a entropy: {:.4f}'.format(ngram_a.test_text_entropy(test_str)))  # noqa
-        print('ngram_b entropy: {:.4f}'.format(ngram_b.test_text_entropy(test_str)))  # noqa
-        print('ngram_c entropy: {:.4f}'.format(ngram_c.test_text_entropy(test_str)))  # noqa
-        print('')
 
-    print(ngram_a.generate(3))
-    print(ngram_b.generate(3))
-    print(ngram_c.generate(3))
+    # test_strings = [
+    #     "The street looks when it's just rained",
+    #     'Something about the way my street looks?',
+    #     'i am Taylor Alison Swift and i love CATS!',
+    #     'asdfasdf jlkdfjk asdf; ',
+    #     "I wanna stay right here, in this passenger's seat",
+    # ]
+    # for test_str in test_strings:
+    #     print('test str:', test_str)
+    #     print('ngram_a entropy: {:.4f}'.format(ngram_a.test_text_entropy(test_str)))  # noqa
+    #     print('ngram_b entropy: {:.4f}'.format(ngram_b.test_text_entropy(test_str)))  # noqa
+    #     print('ngram_c entropy: {:.4f}'.format(ngram_c.test_text_entropy(test_str)))  # noqa
+    #     print('')
+
+    print(ngram_a.generate(10))
+    print(ngram_b.generate(10))
+    print(ngram_c.generate(10))
 
